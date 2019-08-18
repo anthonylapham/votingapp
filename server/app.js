220186c250644cba5f7f6728fa9784101f9b982b
Anthony Lapham <lapham.2@wright.edu>
2019-08-18 10:10:06 -0400

    env file

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,14 +8,16 @@ const app = express();
 
 // MongoDB Database Connection
 /*const db = mongoose.connection;
-const MONGO_URI = 'mongodb://alapham:dirtywars1122@ds263927.mlab.com:63927/votingapp'
-mongoose.connect(MONGO_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 db.once('open', () => console.log('connected to the database'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));*/
 
 // Application Middleware
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(logger('dev'));
 
 app.get('/health', (req, res) => {
